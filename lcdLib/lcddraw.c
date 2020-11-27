387bc80408c0519f58e6137d29693ad1c4e71440
student <sghernandez5@miners.utep.edu>
2020-11-26 20:02:21 -0700

    fixed 8x12 font,it was diagonal

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -103,22 +103,25 @@ void drawString5x7(u_char col, u_char row, char *string,
 void drawChar8x12(u_char rcol, u_char rrow, char c, 
      u_int fgColorBGR, u_int bgColorBGR) 
 {
+  // starting at the MSB
   u_char col = 0;
   u_char row = 0;
-  u_char bit = 0x01;
+  u_char bit = 0x80;
   u_char oc = c - 0x20;
 
-  lcd_setArea(rcol, rrow, rcol + 7, rrow + 12); /* relative to requested col/row */
+  lcd_setArea(rcol, rrow, rcol + 8, rrow + 12); /* relative to requested col/row */
   // 8x12 font 
-  while (row < 12) {
-    while (col < 8) {
-      u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+  while (row < 13) {
+    while (col < 9) {
+      u_int colorBGR = (font_8x12[oc][row] & bit) ? fgColorBGR : bgColorBGR;
       lcd_writeColor(colorBGR);
       col++;
-      //rotate
+      //shift the msb to the right to compare
       bit>>=1;
     }
     col = 0;
+    // set it again to 0x80, so it can compare as it goes down the rows.
+    bit = 0x80; 
     row++;
   }
 }
