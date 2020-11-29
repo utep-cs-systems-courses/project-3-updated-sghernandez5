49fc02829331e575910179da7d8c2f7dbd2ca3b6
student <sghernandez5@miners.utep.edu>
2020-11-29 15:05:26 -0700

    added assembly for dim75

--- project/assembly.s
+++ project/assembly.s
@@ -1,8 +1,13 @@
 
 	.arch msp430g2553
 	.p2align 1,0
-	.text
 
+	
+	.data
+dimState:
+	.byte 0			; dimState = 0
+	
+	.text
 	.extern red_on
 	.extern led_update
 	.extern led_changed
@@ -16,34 +21,30 @@ jt:
 	.global dim75
 	
 dim75:					
-	mov.b #0, R12			; dimState
-	cmp #5, R12			; dimState - 5
+	cmp #4, &dimState		;  dimState - 4
 	jc out 				;
-
-	mov.b R12, R14			; 
-
-	add.b R14, R14			;
-
-	mov  jt(R14), R0		;
+	mov.b &dimState, R12		; 
+	add.b R12,R12
+	mov.b  jt(R12), R0		;
 
 case0:
-	mov.b #0, &red_on		;
-	mov.b #1, R12			;
+	mov.b #1, &red_on		;
+	mov.b #1, &dimState			;
 	jmp  out			; 
 	
 case1:
 	mov.b #1, &red_on		;
-	mov.b #2, R12			;
+	mov.b #2, &dimState		;
 	jmp out				;
 case2:
 	mov.b #1, &red_on		;
-	mov.b #3, R12			;
+	mov.b #3, &dimState			;
 	jmp out				;
 case3:
-	mov.b #1, &red_on		;
-	mov.b #0, R12			;
+	mov.b #0, &red_on		;
+	mov.b #0, &dimState			;
 	jmp out				;
 out:
-	mov #1, &led_changed		;
-	call #led_update		;
-	pop r0				; 
+	mov.b #1, &led_changed		;
+	call #led_update
+	pop R0			; 
